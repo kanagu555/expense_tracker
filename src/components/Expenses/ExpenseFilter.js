272190d7212cea1fcbfa85b44a28137a12a12d43
Kanagaraj <kanagarajwhb@gmail.com>
2021-09-03 22:25:16 +0530

    implemented condition rendering concepts

--- src/components/Expenses/ExpenseFilter.js
+++ src/components/Expenses/ExpenseFilter.js
@@ -12,10 +12,11 @@ const ExpenseFilter = (props) => {
             <div className="expenses-filter__control">
                 <label>Filter by year</label>
                 <select value={props.selected} onChange={dropdownChangeHandler}>
+                    <option value='all'>All</option>
                     <option value="2022">2022</option>
                     <option value="2021">2021</option>
                     <option value="2020">2020</option>
-                    <option value="2019">2012</option>
+                    <option value="2019">2019</option>
                 </select>
 
             </div>
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -11,19 +11,31 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear);
   };
 
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+
+  let expensesContent = <p>No expense found</p>
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ))
+  }
+
   return (
     <Card className="expenses">
       <ExpenseFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      {props.items.map((expense) => (
-        <ExpenseItem
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))}
+      {expensesContent }
     </Card>
   );
 };
